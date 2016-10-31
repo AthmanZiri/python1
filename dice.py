f00ed8459ac73e62c3333de5ed0d5c22a9f7c6bd
athmanziri <issaziri@gmail.com>
2016-10-31 19:56:51 +0300

    python tests

---
+++ dice.py
@@ -0,0 +1,19 @@
+from random import randint
+a=randint(1,6)
+#while a:
+  #print a
+  #print "would you like to print again?"
+print a  
+yes=True
+no=False
+q=input( "would you like to roll again?")
+print q
+if yes:
+ b=randint(1,6)  
+ print b
+elif no:
+   quit()  
+
+     
+
+ 
